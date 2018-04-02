ac02ee35b6b30a0176961988d4c08f75a903ec06
Julien Gaubert <gaubert.julien@gmail.com>
2018-04-02 05:25:06 +0200

    refactoring

--- src/main/java/com/arolla/tennis/Game.java
+++ src/main/java/com/arolla/tennis/Game.java
@@ -1,19 +1,18 @@
 package com.arolla.tennis;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 public class Game {
 
-    private final static String[] points = {"0", "15", "30", "40"};
+    private final static String[] pointsLabel = {"0", "15", "30", "40"};
 
-    private final Player player1;
-    private final Player player2;
-
-    private Score score = new Score();
+    private final List<Player> players;
+    private int points[] = {0, 0};
 
     public Game(Player player1, Player player2) {
-        this.player1 = player1;
-        this.player2 = player2;
+        players = Arrays.asList(player1, player2);
     }
 
     public void point(Player scorer) {
@@ -21,51 +20,43 @@ public class Game {
             throw new IllegalStateException("Cannot play more point: this game is already won by " + winner().get());
         }
 
-        if (scorer.equals(player1)) {
-            score.increaseP1();
-        } else if (scorer.equals(player2)) {
-            score.increaseP2();
-        } else {
+        int scorerIdx = players.indexOf(scorer);
+        if (scorerIdx == -1) {
             throw new IllegalArgumentException(
-                    String.format("Game is between %s and %s. %s cannot score point.", player1, player2, scorer)
+                    String.format("Game is between %s and %s. %s cannot score point.", players.get(0), players.get(1), scorer)
             );
         }
+
+        increase(scorerIdx);
     }
 
     public Optional<Player> winner() {
-        if (score.points1 > points.length - 1) {
-            return Optional.of(player1);
+        if (win(0)) {
+            return Optional.of(players.get(0));
         }
-        if (score.points2 > points.length - 1) {
-            return Optional.of(player2);
+        if (win(1)) {
+            return Optional.of(players.get(1));
         }
         return Optional.empty();
     }
 
+
     public String printScore() {
-        if (score.points1 > points.length - 1) {
-            return "Game: " + player1;
+        if (win(0)) {
+            return "Game: " + players.get(0);
         }
-        if (score.points2 > points.length - 1) {
-            return "Game: " + player2;
+        if (win(1)) {
+            return "Game: " + players.get(1);
         }
-        return points[score.points1] + "-" + points[score.points2];
+        return pointsLabel[points[0]] + "-" + pointsLabel[points[1]];
     }
 
-    /**
-     * Simple helper class to represent the score
-     */
-    private static class Score {
-        private int points1 = 0;
-        private int points2 = 0;
+    private boolean win(int playerIdx) {
+        return points[playerIdx] > pointsLabel.length - 1;
+    }
 
-        public void increaseP1() {
-            points1++;
-        }
-
-        private void increaseP2() {
-            points2++;
-        }
+    private void increase(int playerIdx) {
+        points[playerIdx]++;
     }
 
 }
